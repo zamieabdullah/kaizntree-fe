c6f10b4f72cfe5f03f65c6e0e68a8fb03a6b3893
zamieabdullah <zamieabdullah0@gmail.com>
2024-04-25 18:53:42 -0400

    login and signup created

---
+++ kaizntree-fe/src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container-fluid text-center">
+        <form>
+            <h1 class="mb-5">Kaizntree</h1>
+            <div class="mb-4">
+                <input type="email" 
+                    class="form-control-lg" 
+                    id="exampleInputEmail1" 
+                    placeholder="Email Address" 
+                    aria-describedby="emailHelp"
+                    v-model="email"
+                    required
+                >
+            </div>
+            <div class="mb-4">
+                <input 
+                    type="password" 
+                    class="form-control-lg" 
+                    placeholder="Password"
+                    id="exampleInputPassword1"
+                    v-model="password"
+                    required
+                >
+            </div>
+            <button type="submit" class="btn btn-primary" @click.prevent="handleSignup" :style="{marginRight: '3px'}">Create Account</button>
+            <button type="submit" class="btn btn-primary" @click.prevent="handleLogin" :style="{marginLeft: '3px'}">Log In</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+        }
+    },
+    methods: {
+        async handleLogin(){
+            try {
+                const response = await axios.post('/api/login/', { email_address: this.email, password: this.password });
+                console.log(response); // Handle successful login response
+            } catch (error) {
+                console.error('Login failed'); // Handle login error
+            }
+        },
+        async handleSignup(){
+            try {
+                const response = await axios.post('/api/signup/', { email_address: this.email, password: this.password });
+                console.log(response); // Handle successful login response
+            } catch (error) {
+                console.error(); // Handle login error
+            }
+        }
+    }
+}
+</script>
---
+++ kaizntree-fe/vite.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000', // Change this to your backend server's URL
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
