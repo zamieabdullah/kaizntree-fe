154b5bf1a4c94f73502fb5d878b473fc6e4b98a1
zamieabdullah <zamieabdullah0@gmail.com>
2024-04-26 10:30:12 -0400

    adding categories implemented;

--- kaizntree-fe/src/views/Dashboard.vue
+++ kaizntree-fe/src/views/Dashboard.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="col">
                 <div class="row mb-3">
-                    <h4>Total Categories: 0</h4>
+                    <h4>Total Categories: {{ num_categores }}</h4>
                 </div>
                 <div class="row">
                     <h4>Total Items: 0</h4>
@@ -16,11 +16,98 @@
             </div>
         </div>
         <div class="mt-4">
-            <button class="btn btn-success">New Item Category</button>
+            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#categoryModal">New Item Category</button>
+        </div>
+        <div class="modal fade" id="categoryModal" tabindex="-1" aria-labelledby="categoryModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="categoryModalLabel">Add Item Category</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <input type="text"
+                                class="form-control"
+                                v-model="new_category"
+                                placeholder="New category"
+                            >
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" @click="addCategories" data-bs-dismiss="modal">Save changes</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
-<script setup>
+<script>
 import Header from '../components/Header.vue';
+import axios from 'axios';
+
+export default {
+    components: {
+        Header
+    },
+    data() {
+        return {
+            new_category: "",
+            categories: [],
+            items: []
+        }
+    },
+    computed: {
+        num_categores() {
+            return this.categories ? this.categories.length : 0
+        },
+        num_items() {
+            return this.items ? this.items.length : 0
+        }
+    },
+    methods: {
+        getCookie() {
+            const value = `; ${document.cookie}`;
+            const parts = value.split(`; access_token=`);
+            if (parts.length === 2) return parts.pop().split(';').shift();
+        },
+        async addCategories() {
+            if (this.new_category !== "" && this.new_category !== null) {
+                try {
+                    await axios.post('/api/category/', {
+                        name: this.new_category
+                    }, {
+                        headers: {
+                            'CSRFToken': `Bearer ${this.getCookie()}`
+                        }
+                    }).then((data => {
+                        this.getCatgories();
+                    }));
+                } catch (error) {
+                    console.error(error);
+                }
+            } else {
+                console.error("Need a category name")
+            }
+        },
+        async getCatgories() {
+            try {
+                await axios.get('/api/category/', {
+                    headers: {
+                        'CSRFToken': `Bearer ${this.getCookie()}`
+                    }
+                }).then((data => {
+                    this.categories = data.data
+                }));
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    },
+    mounted() {
+        this.getCatgories();
+    }
+}
 </script>
